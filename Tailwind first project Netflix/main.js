d94d90bc2e7cb65f7467b1f72712252c14f74434
thomasgrouazel <91453814+thomasgrouazel@users.noreply.github.com>
2022-03-16 13:49:33 +0100

    Add files via upload

---
+++ Tailwind first project Netflix/main.js
@@ -0,0 +1,6 @@
+function goRight (bretagne) {
+    document.getElementById(bretagne).scrollLeft += 304;
+};
+function goLeft (normandie) {
+    document.getElementById(normandie).scrollLeft -= 304;
+};
