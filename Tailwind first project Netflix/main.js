5e7410a9b15b12d7a03a6c5375f294081910d233
thomasgrouazel <91453814+thomasgrouazel@users.noreply.github.com>
2022-03-19 20:54:34 +0100

    Delete Tailwind first project Netflix directory

--- Tailwind first project Netflix/main.js
+++
@@ -1,6 +0,0 @@
-function goRight (bretagne) {
-    document.getElementById(bretagne).scrollLeft += 304;
-};
-function goLeft (normandie) {
-    document.getElementById(normandie).scrollLeft -= 304;
-};
